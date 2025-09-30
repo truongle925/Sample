f6c42eda24261421438544edb20c90e35dbf93de
truongle925 <130240979+truongle925@users.noreply.github.com>
2025-09-30 21:07:28 +0700

    Hot fix

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {DemoComponent} from './shared/demo-component/demo-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DemoComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
